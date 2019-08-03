0133360bc4141c3546c0bd66a06f3642d493d7c0
Evan  Markert <evanmarkert@gmail.com>
2019-08-03 20:53:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendDriver() {
-  return drivers.push();
+function destructivelyAppendDriver(name) {
+  return drivers.push(name);
 }
